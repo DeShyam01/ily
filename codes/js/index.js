693c3296980e60edb1a5d9f2a35ca57638635b25
DeShyam01 <shyamsundargayen345@gmail.com>
2025-05-23 18:01:36 +0530

    Delete codes directory

--- codes/js/index.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const flap = document.querySelector(".flap");
-  flap.addEventListener("click", function () {
-    flap.classList.toggle("open");
-  });
-});
